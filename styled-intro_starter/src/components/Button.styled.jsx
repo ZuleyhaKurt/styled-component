df8609984ce87bd201452a110f07f49e36c3e0dd
Züleyha Kurt <netice2017@gmail.com>
2022-10-25 20:37:07 +0300

    buttonjsx

---
+++ styled-intro_starter/src/components/Button.styled.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+background-color: hotpink;
+color: white;
+cursor: pointer;
+padding:1rem;
+border-radius: 0.5rem;
+border-color:pink;
+&:hover{
+    transform: scale(0.97);
+}
+
+`
+
+export default Button
