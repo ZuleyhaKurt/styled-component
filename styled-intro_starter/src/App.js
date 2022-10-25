fca918c93bedaf14a2c7a1b7ba1c96882b7dd499
Züleyha Kurt <netice2017@gmail.com>
2022-10-25 21:29:09 +0300

    styled

--- styled-intro_starter/src/App.js
+++ styled-intro_starter/src/App.js
@@ -1,6 +1,7 @@
 import Button, { TomatoButton } from "./components/Button.styled";
 import Container from "./components/Container";
 import HeaderText from "./components/HeaderText.styled";
+import StyledLink from "./components/Link.styled";
 
 const App = () => {
   return (
@@ -12,8 +13,8 @@ const App = () => {
       <TomatoButton>Send</TomatoButton>
       <TomatoButton primary>Submit</TomatoButton>
       </Container>
-      <Container>
-        <StyledLink href="https://clarusway.com/" target="_blanck"></StyledLink>
+      <Container bg="#eee">
+        <StyledLink href="https://clarusway.com/" target="_blanck">Clarusway</StyledLink>
       </Container>
     
       
--- styled-intro_starter/src/components/Link.styled.jsx
+++ styled-intro_starter/src/components/Link.styled.jsx
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 const StyledLink= styled.a`
  font-size:1rem;
  text-decoration: none;
+ color: black;
  &:hover{
-    
+    opacity:0.5;
  }
 `;
 
