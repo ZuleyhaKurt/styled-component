a55542751f6c9a7d89aab1154b70f137b6f2aaaf
Züleyha Kurt <netice2017@gmail.com>
2022-10-25 21:26:10 +0300

    link

--- styled-intro_starter/src/App.js
+++ styled-intro_starter/src/App.js
@@ -1,15 +1,20 @@
 import Button, { TomatoButton } from "./components/Button.styled";
 import Container from "./components/Container";
+import HeaderText from "./components/HeaderText.styled";
 
 const App = () => {
   return (
     <>
       <Container bg="gray">
+        <HeaderText color> STYLED COMPONENTS</HeaderText>
   <Button primary>Click</Button>
       <Button>Save</Button>
       <TomatoButton>Send</TomatoButton>
       <TomatoButton primary>Submit</TomatoButton>
-    </Container>
+      </Container>
+      <Container>
+        <StyledLink href="https://clarusway.com/" target="_blanck"></StyledLink>
+      </Container>
     
       
     </>
---
+++ styled-intro_starter/src/components/Link.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledLink= styled.a`
+ font-size:1rem;
+ text-decoration: none;
+ &:hover{
+    
+ }
+`;
+
+export default StyledLink;
